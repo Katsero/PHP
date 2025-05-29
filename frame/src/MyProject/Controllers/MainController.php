010f5d1ecb1140850037754850c75cf32f1dc3f6
Katsero <raleksandrd25@gmail.com>
2025-05-29 16:04:27 +0300

    first controller

---
+++ frame/src/MyProject/Controllers/MainController.php
@@ -0,0 +1,31 @@
+<?php
+
+ 
+
+namespace MyProject\Controllers;
+
+ 
+
+class MainController
+
+{
+
+    public function main()
+
+    {
+
+        echo 'Главная страница';
+
+    }
+
+ 
+
+    public function sayHello(string $name)
+
+    {
+
+        echo 'Привет, ' . $name;
+
+    }
+
+}
