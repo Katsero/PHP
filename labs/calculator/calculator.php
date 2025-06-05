5b1592eca28cc68d9f26efb73e57be1e985a3a54
Katsero <raleksandrd25@gmail.com>
2025-06-05 21:58:45 +0300

    calculator fix

--- labs/calculator/calculator.php
+++ labs/calculator/calculator.php
@@ -1,4 +1,5 @@
 <?php
+
 $expression = $_POST['expression'] ?? '';
 
 if (empty($expression)) {
@@ -6,19 +7,99 @@ if (empty($expression)) {
     exit;
 }
 
+// Удаляем лишние пробелы
+$expression = str_replace(' ', '', $expression);
+
 try {
-    $result = eval("return $expression;");
+    // Парсим и вычисляем выражение
+    $tokens = tokenize($expression);
+    $result = parseExpression($tokens);
 
     if (!is_numeric($result)) {
         throw new Exception("Неверное выражение");
     }
 
     echo $result;
-} catch (DivisionByZeroError $e) {
-    echo "Ошибка: Деление на ноль недопустимо";
-} catch (ParseError $e) {
-    echo "Ошибка: Некорректное выражение";
+
 } catch (Exception $e) {
     echo "Ошибка: " . $e->getMessage();
 }
-?>
+
+// ==== Лексический анализатор ====
+function tokenize($expr) {
+    $tokens = [];
+    $i = 0;
+    $len = strlen($expr);
+
+    while ($i < $len) {
+        $char = $expr[$i];
+
+        if (in_array($char, ['+', '-', '*', '/', '(', ')', '.'])) {
+            $tokens[] = $char;
+            $i++;
+        } elseif (is_numeric($char)) {
+            $num = $char;
+            $i++;
+
+            while ($i < $len && (is_numeric($expr[$i]) || $expr[$i] === '.')) {
+                $num .= $expr[$i];
+                $i++;
+            }
+
+            $tokens[] = $num;
+        } else {
+            throw new Exception("Неизвестный символ: $char");
+        }
+    }
+
+    return $tokens;
+}
+
+// ==== Рекурсивный парсер ====
+function parseExpression(&$tokens) {
+    $result = parseTerm($tokens);
+
+    while (!empty($tokens) && in_array($tokens[0], ['+', '-'])) {
+        $op = array_shift($tokens);
+        $right = parseTerm($tokens);
+        if ($op === '+') $result += $right;
+        else $result -= $right;
+    }
+
+    return $result;
+}
+
+function parseTerm(&$tokens) {
+    $result = parseFactor($tokens);
+
+    while (!empty($tokens) && in_array($tokens[0], ['*', '/'])) {
+        $op = array_shift($tokens);
+        $right = parseFactor($tokens);
+
+        if ($op === '*') $result *= $right;
+        else {
+            if ($right == 0) {
+                throw new Exception("Деление на ноль недопустимо");
+            }
+            $result /= $right;
+        }
+    }
+
+    return $result;
+}
+
+function parseFactor(&$tokens) {
+    $token = array_shift($tokens);
+
+    if ($token === '(') {
+        $result = parseExpression($tokens);
+        if (array_shift($tokens) !== ')') {
+            throw new Exception("Отсутствует закрывающая скобка");
+        }
+        return $result;
+    } elseif (is_numeric($token)) {
+        return floatval($token);
+    } else {
+        throw new Exception("Ожидалось число или '('");
+    }
+}
